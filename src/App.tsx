5ceee2e30f346f5c9a0465c165c434dfaea3c65e
dsouzajoy <joydsouza3380@gmail.com>
2022-09-03 14:20:42 +0530

    Created env based webpack configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import './App.css'
+import backgroundImage from './background.jpeg'
+import TwitterIcon from './twitter.svg'
+
 const App = () => (
-    <h1>hello world</h1>
+    <>
+        <h1>hello world</h1>
+        <img src={backgroundImage} />
+        <div className='logo'>
+            <TwitterIcon/>
+        </div>
+    </>
 )
 
 export default App
